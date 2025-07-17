67e7ab2f8f0588d605f227329243a09300e46320
Shenan Vindinu <shenanvindinu@gmail.com>
2025-07-17 17:23:45 +0530

    feat : implemented subdomain based multi database tenancy to the sample project

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,17 @@
 <?php
 
-use App\Http\Controllers\PlaceController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is for your central application routes. These routes are
+| not tenant-specific. All authenticated routes should be in routes/tenant.php.
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// The dashboard route pointing to our new controller
-Route::get('/dashboard', [PlaceController::class, 'index'])
-    ->middleware(['auth', 'verified'])->name('dashboard');
-
-// All other authenticated routes
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // Routes to handle adding/deleting places
-    Route::resource('places', PlaceController::class)->except(['index']);
-});
-
-require __DIR__.'/auth.php';
